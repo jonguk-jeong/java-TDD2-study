2d6d585a44b29ddb65a61d051f2f92c6e462fa61
깃허브아이디 <>
2026-02-05 15:51:27 +0900

    4 - 출력을 모니터 대신 문자열로 받는 실험

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
+        lab2();
+    }
+
+    public static void lab2() {
+
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); // 출력 여기에 저장 됨
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("hihi");
+        System.out.println("byebye");
+        System.out.print("ok");
+
+        System.setOut(ORIGINAL_OUT); // 모니터로 출력 복원
+
+        String result = byteArrayOutputStream.toString();
+
+        System.out.println(result);
+    }
+
+    public static void lab1() {
         Scanner scan = new Scanner("""
                 등록
                 과거에 집착하지 마라.
@@ -13,8 +38,10 @@ public class Main {
         String str2 = scan.nextLine();
         String str3 = scan.nextLine();
 
+
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
+
     }
 }
