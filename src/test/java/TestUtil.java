27b3179ec0ca248e58cac3f0729ba5086fa5a323
깃허브아이디 <>
2026-02-06 11:18:30 +0900

    6 - b

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,19 +1,31 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
 
-
-    //    public static String run(String input) {
-//
-//        // 입력값
-//        Scanner scan = new Scanner(input);
-//
-//        // 명언 프로그램 실행
-//
-//        // 출력값
-//    }
+    private static PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
 
     public static Scanner genScanner(String input) {
         return new Scanner(input);
+    }
+
+    // 출력을 모니터가 아닌 다른데로 돌리는 기능
+    public static ByteArrayOutputStream setOutByteArray() {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);   // 여기에 입력 쌓임
+
+        System.setOut(printStream);
+        CURRENT_OUT = printStream;
+
+        return byteArrayOutputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws Exception {
+        System.setOut(ORIGINAL_OUT);
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -27,30 +27,22 @@ public class TestUtilTest {
 
     @Test
     @DisplayName("TestUtil.setOutByteArray()")
-    void t2() {
+    void t2() throws Exception{
+        // byteArrayOutputStream 여기에 내 모든 입력 쌓임
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray(); // 출력을 내가 잘 돌려서 받아 올 수 있나 테스트
 
-        System.out.println("안녕하세요");
+        System.out.println("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
 
         String rst = byteArrayOutputStream.toString();
 
-        assertThat(rst).isEqualTo("안녕하세요");
+        // 모니터로 확인하고 싶은 시점
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        System.out.println("출력 결과 : " + rst);
+
+        assertThat(rst).contains("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        //assertThat(rst).contains("안녕하세요"); // isEqualTo는 정확히 일치해야함 너무 엄격. 그래서 contains로 바꿈
 
     }
-
-//    @Test
-//    @DisplayName("등록")
-//    void t1() {
-//
-//        final String out = TestUtil.run("""
-//                등록
-//                현재를 사랑하라.
-//                작자미상
-//                """);
-//
-//        assertThat(out)
-//                .contains("명언 :")
-//                .contains("작가 :")
-//                .contains("1번 명언이 등록되었습니다.");
-//    }
 }
