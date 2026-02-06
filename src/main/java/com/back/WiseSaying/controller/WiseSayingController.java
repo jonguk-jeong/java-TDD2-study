78db5f8025884960d55a1e01ead2fa6e0e3a0fa8
깃허브아이디 <>
2026-02-06 15:33:22 +0900

    20 - r, g

--- src/main/java/com/back/WiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/WiseSaying/controller/WiseSayingController.java
@@ -45,9 +45,26 @@ public class WiseSayingController {
     public void actionDelete(Rq rq) {
 
         int id = rq.getParamAsInt("id", -1);
-        boolean rst = wiseSayingService.delete(id);
+        boolean deleted = wiseSayingService.delete(id);
 
+        if(!deleted) {
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
         System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
+
+    }
+
+    public void actionModify(Rq rq) {
+
+        int id = rq.getParamAsInt("id", -1);
+
+        WiseSaying wiseSaying = wiseSayingService.findByIdOrNull(id);
+
+        if(wiseSaying == null) {
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
 
     }
 
