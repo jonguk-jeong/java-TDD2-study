46ca271beb878039ccb5005d9cdb23e8686a36fc
깃허브아이디 <>
2026-02-06 12:50:20 +0900

    14 - b

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
-
-    int lastId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
+    private WiseSayingController wiseSayingController;
+    private SystemController systemController;
 
     public App(Scanner sc) {
+//        this.scanner = new Scanner(System.in);  이거는 실제 입력을 해야만 함
         this.sc = sc; // 일단은 테스트 케이스 돌리기 위한 용도로 만듬
-        // this.scanner = new Scanner(System.in);  이거는 실제 입력을 해야만 함
+        this.wiseSayingController = new WiseSayingController(sc);
+        this.systemController = new SystemController();
     }
 
     public void run() {
@@ -19,36 +18,16 @@ public class App {
 
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
+        while (true) {
 
             System.out.println("명령) ");
             String cmd = sc.nextLine();
 
             switch (cmd) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String saying = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-
-                    lastId++;
-
-                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
-                    wiseSayings.add(wiseSaying);
-
-                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-                }
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
-                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
-                }
+                case "등록" -> wiseSayingController.actionAdd();
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
+                    systemController.actionExit();
                     return;
                 }
             }
--- src/main/java/SystemController.java
+++ src/main/java/SystemController.java
@@ -1,3 +1,5 @@
 public class SystemController {
-
+    public void actionExit() {
+        System.out.println("프로그램을 종료합니다.");
+    }
 }
