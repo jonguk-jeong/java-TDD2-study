a4e340e982931e2d0b57117d76239dcb5010fe45
깃허브아이디 <>
2026-02-06 11:46:03 +0900

    9 - r

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,10 +7,24 @@ public class AppTest {
 
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
-    void t1() throws Exception {
+    void t1() {
         String out = AppTestRunner.run("종료");   // 앱을 대신 실행해 주는 용도
 
         assertThat(out).contains("== 명언 앱 ==");
 
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언 : ");
+        assertThat(out).contains("작가 : ");
     }
 }
